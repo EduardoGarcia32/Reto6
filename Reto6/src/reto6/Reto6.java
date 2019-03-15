1e43028111d6731076733fca12104e50f8d1391b
EduardoGarcia32 <slender560@gmail.com>
2019-03-14 22:05:22 -0600

    Periodo de ventas

--- Reto6/src/reto6/Reto6.java
+++ Reto6/src/reto6/Reto6.java
@@ -21,6 +21,34 @@ int [][] datosmasaltos = {{0,0,0,0},{0,0,0,0}};
 
 String [] trimestre = {"Enero a Marzo", "Abril a Junio", "Julio a Septiembre", "Octubre a Diciembre"};
 
+for( int n = 0; n<datos.length; n++){
+    for( int i = 0; i<datos.length; i++){
+        if(datos[i][n]>datosmasaltos[0][n]){
+            datosmasaltos[0][n] = datos[i][n];
+            datosmasaltos[1][n] = i;
+        }
+    }
+} 
+        System.out.println("El periodo donde más se vendio el sabor de chocolate fue: " +trimestre[datosmasaltos[1][0]]);
+        System.out.println("El periodo donde más se vendio el sabor de vainilla fue: " +trimestre[datosmasaltos[1][1]]);
+        System.out.println("El periodo donde más se vendio el sabor de fresa fue: " +trimestre[datosmasaltos[1][2]]);
+        System.out.println("El periodo donde más se vendio el sabor de oreo fue: " +trimestre[datosmasaltos[1][3]]);
 
-    }  
+            int[] totaltrimestres = {0,0,0,0,};
+            int [][] trimestreconmasventa = {{0},{0}};
+        
+        for( int n = 0; n<datos.length; n++){
+            for( int i = 0; i<datos.length; i++){
+                totaltrimestres[n] += datos [n][i];
+    }   
+} 
+    for (int n = 0; n<datos.length; n++){
+        if(totaltrimestres[n]>trimestreconmasventa[0][0]){
+            trimestreconmasventa[0][0] = totaltrimestres[n];
+            trimestreconmasventa [1][0] = n;
+            }
+        }
+        System.out.println("El trimestre que tubo mayor venta de producto fue : "+trimestre[trimestreconmasventa[1][0]]);
+    }
+    
 }
